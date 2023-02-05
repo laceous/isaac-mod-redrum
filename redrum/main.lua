403c15ae53d5fcfcce50c8e61d7d190472b94de0
laceous <laceous@gmail.com>
2023-02-04 22:13:01 -0800

    remove stageSeeds

--- redrum/main.lua
+++ redrum/main.lua
@@ -5,35 +5,19 @@ local game = Game()
 mod.text = 'REDЯUM'
 
 mod.state = {}
-mod.state.stageSeeds = {} -- per stage
 mod.state.roomCounts = {} -- per stage/type
 
 function mod:onGameStart(isContinue)
-  local level = game:GetLevel()
-  local stage = level:GetStage()
-  local seeds = game:GetSeeds()
-  local stageSeed = seeds:GetStageSeed(stage)
-  mod:setStageSeed(stageSeed)
   mod:setRoomCount(-1)
   
   if isContinue and mod:HasData() then
     local _, state = pcall(json.decode, mod:LoadData())
     
     if type(state) == 'table' then
-      if type(state.stageSeeds) == 'table' then
-        -- quick check to see if this is the same run being continued
-        if state.stageSeeds[tostring(stage)] == stageSeed then
-          for key, value in pairs(state.stageSeeds) do
-            if type(key) == 'string' and math.type(value) == 'integer' then
-              mod.state.stageSeeds[key] = value
-            end
-          end
-          if type(state.roomCounts) == 'table' then
-            for key, value in pairs(state.roomCounts) do
-              if type(key) == 'string' and math.type(value) == 'integer' then
-                mod.state.roomCounts[key] = value
-              end
-            end
+      if type(state.roomCounts) == 'table' then
+        for key, value in pairs(state.roomCounts) do
+          if type(key) == 'string' and math.type(value) == 'integer' then
+            mod.state.roomCounts[key] = value
           end
         end
       end
@@ -44,20 +28,14 @@ end
 function mod:onGameExit(shouldSave)
   if shouldSave then
     mod:SaveData(json.encode(mod.state))
-    mod:clearStageSeeds()
     mod:clearRoomCounts()
   else
-    mod:clearStageSeeds()
     mod:clearRoomCounts()
     mod:SaveData(json.encode(mod.state))
   end
 end
 
 function mod:onNewLevel()
-  local level = game:GetLevel()
-  local seeds = game:GetSeeds()
-  local stageSeed = seeds:GetStageSeed(level:GetStage())
-  mod:setStageSeed(stageSeed)
   mod:setRoomCount(-1)
 end
 
@@ -118,17 +96,6 @@ function mod:getStageIndex()
   end
   
   return game:GetVictoryLap() .. '-' .. stage .. '-' .. stageType .. '-' .. (isAltStage and 1 or 0) .. '-' .. (level:IsPreAscent() and 1 or 0) .. '-' .. (level:IsAscent() and 1 or 0)
-end
-
-function mod:setStageSeed(seed)
-  local level = game:GetLevel()
-  mod.state.stageSeeds[tostring(level:GetStage())] = seed
-end
-
-function mod:clearStageSeeds()
-  for key, _ in pairs(mod.state.stageSeeds) do
-    mod.state.stageSeeds[key] = nil
-  end
 end
 
 mod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, mod.onGameStart)
